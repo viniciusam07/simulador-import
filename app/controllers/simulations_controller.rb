ab0f1b913c2683367f023da5a91e11fe64d29424
Vinicius Marques <v.alvesmarques@gmail.com>
2024-12-17 13:24:26 -0300

    Fix Expense simulation Values

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -8,37 +8,14 @@ class SimulationsController < ApplicationController
     @simulation.user_id = current_user.id
 
     if @simulation.save
-      # Associa despesas selecionadas, evitando duplicação
-      selected_expenses = Expense.where(id: params[:simulation][:expense_ids])
-      selected_expenses.each do |expense|
-        @simulation.simulation_expenses.find_or_create_by!(expense: expense) do |sim_expense|
-          sim_expense.expense_custom_name = expense.expense_name
-          sim_expense.expense_custom_value = expense.percentage ? (expense.percentage * @simulation.freight_cost / 100.0) : expense.expense_default_value
-          sim_expense.expense_currency = expense.expense_currency
-          sim_expense.expense_location = expense.expense_location
-        end
-      end
-
-      # Adiciona AFRMM automaticamente apenas se não existir
-      if @simulation.modal == 'Marítimo'
-        afrmm = Expense.find_by(expense_name: 'AFRMM')
-        if afrmm
-          @simulation.simulation_expenses.find_or_create_by!(expense: afrmm) do |sim_expense|
-            sim_expense.expense_custom_name = afrmm.expense_name
-            sim_expense.expense_custom_value = afrmm.percentage ? (afrmm.percentage * @simulation.freight_cost / 100.0) : afrmm.expense_default_value
-            sim_expense.expense_currency = afrmm.expense_currency
-            sim_expense.expense_location = afrmm.expense_location
-          end
-        end
-      end
+      attach_selected_expenses
+      attach_afrmm_if_needed
 
       redirect_to simulation_path(@simulation), notice: 'Simulação criada com sucesso.'
     else
       render :new, alert: 'Erro ao criar a simulação.'
     end
   end
-
-
 
   def index
     @simulations = Simulation.where(user_id: current_user.id)
@@ -59,6 +36,54 @@ class SimulationsController < ApplicationController
   private
 
   def simulation_params
-    params.require(:simulation).permit(:origin_country, :total_value, :incoterm, :modal, :currency, :exchange_rate, :freight_cost, :insurance_cost, :aliquota_ii, :tributo_ii, :aliquota_ipi, :tributo_ipi, :aliquota_pis, :tributo_pis, :aliquota_cofins, :tributo_cofins, :aliquota_icms, :tributo_icms, expense_ids: [])
+    params.require(:simulation).permit(
+      :origin_country, :total_value, :incoterm, :modal, :currency, :exchange_rate,
+      :freight_cost, :insurance_cost, :aliquota_ii, :tributo_ii, :aliquota_ipi, :tributo_ipi,
+      :aliquota_pis, :tributo_pis, :aliquota_cofins, :tributo_cofins, :aliquota_icms, :tributo_icms,
+      expense_ids: []
+    )
+  end
+
+  def attach_selected_expenses
+    selected_expenses = Expense.where(id: params[:simulation][:expense_ids])
+    selected_expenses.each do |expense|
+      @simulation.simulation_expenses.find_or_create_by!(expense: expense) do |sim_expense|
+        sim_expense.expense_custom_name = expense.expense_name
+        sim_expense.expense_custom_value = calculate_expense_value(expense)
+        sim_expense.expense_currency = expense.expense_currency
+        sim_expense.expense_location = expense.expense_location
+      end
+    end
+  end
+
+  def attach_afrmm_if_needed
+    return unless @simulation.modal == 'Marítimo'
+
+    afrmm = Expense.find_by(expense_name: 'AFRMM')
+    return unless afrmm
+
+    @simulation.simulation_expenses.find_or_create_by!(expense: afrmm) do |sim_expense|
+      sim_expense.expense_custom_name = afrmm.expense_name
+      sim_expense.expense_custom_value = calculate_expense_value(afrmm)
+      sim_expense.expense_currency = afrmm.expense_currency
+      sim_expense.expense_location = afrmm.expense_location
+    end
+  end
+
+  def calculate_expense_value(expense)
+    if expense.percentage.present?
+      case expense.calculation_base
+      when 'freight_cost'
+        @simulation.freight_cost * (expense.percentage / 100.0)
+      when 'customs_value'
+        @simulation.customs_value * (expense.percentage / 100.0)
+      when 'total_value'
+        @simulation.total_value * (expense.percentage / 100.0)
+      else
+        0
+      end
+    else
+      expense.expense_default_value
+    end
   end
 end
--- app/models/simulation_expense.rb
+++ app/models/simulation_expense.rb
@@ -4,15 +4,27 @@ class SimulationExpense < ApplicationRecord
 
   before_validation :set_defaults_from_expense, if: -> { expense.present? }
 
-  def calculate_percentage_value(base_value)
-    expense.percentage ? (base_value * expense.percentage / 100.0) : expense.expense_default_value
+  def calculate_custom_value
+    return expense.expense_default_value if expense.percentage.blank?
+
+    base_value = case expense.calculation_base
+                 when 'freight_cost'
+                   simulation&.freight_cost.to_f
+                 when 'customs_value'
+                   simulation&.customs_value.to_f
+                 when 'total_value'
+                   simulation&.total_value.to_f
+                 else
+                   0
+                 end
+    base_value * (expense.percentage / 100.0)
   end
 
   private
 
   def set_defaults_from_expense
     self.expense_custom_name ||= expense.expense_name
-    self.expense_custom_value ||= calculate_percentage_value(simulation.freight_cost) if expense.percentage
+    self.expense_custom_value ||= calculate_custom_value
     self.expense_currency ||= expense.expense_currency
     self.expense_location ||= expense.expense_location
   end
