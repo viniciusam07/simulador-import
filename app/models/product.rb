eb25800abc64cf76694434d5433ea6f5c3eed631
Vinicius Marques <v.alvesmarques@gmail.com>
2025-01-07 16:44:12 -0300

    Create Standard Measure Products Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ApplicationRecord
+  UNIT_OF_MEASURE_OPTIONS = [
+    ['Grama', 'G'],
+    ['Jogo', 'JOGO'],
+    ['Litro', 'LT'],
+    ['Megawatt hora', 'MWHORA'],
+    ['Metro', 'METRO'],
+    ['Metro cúbico', 'M3'],
+    ['Metro quadrado', 'M2'],
+    ['Mil unidades', '1000UN'],
+    ['Pares', 'PARES'],
+    ['Quilate', 'QUILAT'],
+    ['Quilograma', 'KG'],
+    ['Unidade', 'UN']
+  ].freeze
   has_many :quotations, dependent: :destroy
   # Validações para garantir que os campos principais estejam sempre presentes
   validates :product_name, :unit_of_measure, :unit_price, :ncm, presence: true
